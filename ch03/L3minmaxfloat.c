79cc15fd3dd7e1046f276c7636ac1b6ea5034393
tkWhqkq <qnftp26@naver.com>
2016-09-27 20:43:57 +0900

    소스추가

---
+++ ch03/L3minmaxfloat.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <float.h>
+
+
+int main(void)
+{
+
+	printf("float 범위: %e %e\n", FLT_MIN, FLT_MAX);
+	printf("double 범위: %e\n", FLT_MIN, FLT_MAX); \
+		printf("long double 범위: %e\n", LDBL_MIN, LDBL_MAX);
+
+
+	return 0;
+
+}
